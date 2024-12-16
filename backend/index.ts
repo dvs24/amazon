7f6e546169a5411ecc9b556a52e800a9759b2a42
DVS <divyesh220998@gmail.com>
2024-12-16 21:25:24 +0530

    Made change for cors

--- backend/index.ts
+++ backend/index.ts
@@ -2,8 +2,8 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import productRoutes from "./routes/products"
-import authRoutes from "./routes/auth"
+import productRoutes from "./routes/products";
+import authRoutes from "./routes/auth";
 
 dotenv.config();
 
@@ -12,30 +12,32 @@ const port = process.env.PORT || 8000;
 
 // Middleware setup
 app.use(express.json());
-app.use(cors({
-  origin: (origin, callback) => {
-    // Allow any request from the same domain (but different ports)
-    if (origin && origin.includes("pike.replit.dev")) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      // Allow any request from the same domain (but different ports)
+      if (origin && origin.includes("pike.replit.dev")) {
+        callback(null, true);
+      } else {
+        callback(null, true);
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 // Connection string
 const connectionUrl = process.env.MONGODB_URL as string;
 
 mongoose
-.connect(connectionUrl)
-.then(() => {
-  console.log("Connected to MongoDB");
-})
-.catch((error) => {
-  console.error("Error connecting to MongoDB:", error);
-});
+  .connect(connectionUrl)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
 
 // Routes
 app.use("/products", productRoutes);

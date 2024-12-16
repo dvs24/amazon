03426488544a0d3a43df09699dbe8804fd55b201
DVS <divyesh220998@gmail.com>
2024-12-16 21:59:22 +0530

    updated cors policy

--- backend/index.ts
+++ backend/index.ts
@@ -12,20 +12,21 @@ const port = process.env.PORT || 8000;
 
 // Middleware setup
 app.use(express.json());
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      // Allow any request from the same domain (but different ports)
-      if (origin && origin.includes("pike.replit.dev")) {
-        callback(null, true);
-      } else {
-        callback(null, true);
-      }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       // Allow any request from the same domain (but different ports)
+//       if (origin && origin.includes("pike.replit.dev")) {
+//         callback(null, true);
+//       } else {
+//         callback(null, true);
+//       }
+//     },
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//   })
+// );
+app.use(cors());
 
 // Connection string
 const connectionUrl = process.env.MONGODB_URL as string;
